2886f827abcf7295821e4bf2c62cee248a403b35
DevChristianMorris <devchristianmorris@gmail.com>
2024-03-03 14:21:25 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test React App</h1>
+      <p>Welcome to this app</p>
+      <p>It's a simple app to practice React</p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
