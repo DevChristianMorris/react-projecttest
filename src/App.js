b3cfa2baa198d76f93c7f19c2876ec90783181a0
DevChristianMorris <devchristianmorris@gmail.com>
2024-03-03 14:25:01 +1300

    test commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Test React App</h1>
       <p>Welcome to this app</p>
+      <p>It's a simple testtest practice React</p>
+      <break></break>
       <p>It's a simple app to practice React</p>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
